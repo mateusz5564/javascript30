09998b89f7e51bfa512cd5cddbc7274ef82f4a0a
mateusz5564 <mateusz5564@gmail.com>
2020-08-17 20:18:13 +0200

    custom time form reset, timer in the page title, code refactoring

--- main.js
+++ main.js
@@ -2,47 +2,47 @@ const timerButtons = document.querySelectorAll('.timer__button');
 const customTime = document.querySelector('#custom');
 const timeLeftContainer = document.querySelector('.display__time-left');
 const endTimeContainer = document.querySelector('.display__end-time');
-let currentInterval;
+let currentCountdown;
 
 
 function countdown(timeLeftInSeconds) {
+  if (timeLeftInSeconds <= 0 || !Number.isInteger(Number(timeLeftInSeconds))) return;
+  if (currentCountdown) clearInterval(currentCountdown);
+  displayEndTime(timeLeftInSeconds);
   timeLeftContainer.innerHTML = displayTimeLeft(timeLeftInSeconds);
-  currentInterval = setInterval(() => {
+
+  currentCountdown = setInterval(() => {
     timeLeftInSeconds--;
     timeLeftContainer.innerHTML = displayTimeLeft(timeLeftInSeconds);
-    if(timeLeftInSeconds <= 0) clearInterval(currentInterval);
+    if(timeLeftInSeconds <= 0) clearInterval(currentCountdown);
   }, 1000)
 }
 
 function displayTimeLeft(timeLeftInSeconds) {
-  if (timeLeftInSeconds <= 0) return "0:00";
   const minutesLeft = (timeLeftInSeconds - timeLeftInSeconds % 60) / 60;
   const secondsLeft = timeLeftInSeconds % 60;
-  return `${minutesLeft}:${secondsLeft < 10 ? "0" + secondsLeft : secondsLeft}`;
+  const display = `${minutesLeft}:${secondsLeft < 10 ? "0" + secondsLeft : secondsLeft}`;
+  document.title = display;
+  return display;
 }
 
 function displayEndTime(timeLeftInSeconds) {
-  const date = Date.now();
-  const endTime = new Date(date + Number(timeLeftInSeconds * 1000));
+  const endTime = new Date(Date.now() + Number(timeLeftInSeconds * 1000));
   const hour = endTime.getHours();
   const minute = endTime.getMinutes();
-  endTimeContainer.innerHTML = `${hour}:${minute}`;
+  endTimeContainer.innerHTML = `${hour}:${minute < 10 ? "0" + minute : minute}`;
 }
 
 function handleButton(e) {
-  if(currentInterval) clearInterval(currentInterval);
   const timeLeftInSeconds = e.target.dataset.time;
   countdown(timeLeftInSeconds);
-  displayEndTime(timeLeftInSeconds);
 }
 
 function handleCustomTime(e) {
   e.preventDefault();
-  if(currentInterval) clearInterval(currentInterval);
   const timeLeftInMinutes = e.target[0].value;
-  const timeLeftInSeconds = timeLeftInMinutes * 60;
-  countdown(timeLeftInSeconds);
-  displayEndTime(timeLeftInSeconds);
+  countdown(timeLeftInMinutes * 60);
+  this.reset();
 }
 
 timerButtons.forEach(btn => btn.addEventListener('click', handleButton));
